1f229f9862fc7129ebe4dbd580bfe0d8d39ce091
Fawad <fawad_12@outlook.com>
2019-12-08 22:02:14 +0500

    JSF InputText and SelectOneMenu

---
+++ src/beans/Student.java
@@ -0,0 +1,48 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import java.util.List;
+
+@ManagedBean
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private List<String> countryOptions;
+
+    public Student() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<String> getCountryOptions() {
+        return countryOptions;
+    }
+
+    public void setCountryOptions(List<String> countryOptions) {
+        this.countryOptions = countryOptions;
+    }
+}
